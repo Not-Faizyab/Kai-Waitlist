eb1e67cdd0b5c331a7591a3b92c6ac8fa7446f1b
Faizyab <notfaizyab@gmail.com>
2025-10-03 23:57:22 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+    apiKey: "AIzaSyBX5Tq8Sz3J6A0Or6epm0Y-OGehwdg6dNA",
+    authDomain: "kairo-84926.firebaseapp.com",
+    projectId: "kairo-84926",
+    storageBucket: "kairo-84926.firebasestorage.app",
+    messagingSenderId: "169054130523",
+    appId: "1:169054130523:web:1ce73cf5e928a864854a7e",
+    measurementId: "G-LE9GSPY393"
+};
